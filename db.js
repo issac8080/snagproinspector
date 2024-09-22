4b84faa31420b85f15d309652c96870a0bd4a1c5
issac8080 <98963875+issac8080@users.noreply.github.com>
2024-09-22 20:42:34 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',  // replace with your MySQL username
-    password: 'Issac@2003', // replace with your MySQL password
-    database: 'booking_db'
+    host: process.env.DB_HOST || 'localhost',  // Use the environment variable or default to localhost
+    user: process.env.DB_USER || 'root',      // Use the environment variable or default to 'root'
+    password: process.env.DB_PASSWORD || 'Issac@2003', // Use the environment variable or default to your password
+    database: process.env.DB_DATABASE || 'booking_db'  // Use the environment variable or default to 'booking_db'
 });
 
 connection.connect((err) => {
